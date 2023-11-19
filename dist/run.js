3694e75963b80b7beda7d89ba539f139e9c95f3e
x71c9 <108585118+x71c9@users.noreply.github.com>
2023-11-19 11:48:50 +0100

    changed from esmodule to commonjs

--- dist/run.js
+++ dist/run.js
@@ -1,9 +1,17 @@
+"use strict";
 /**
  *
  * Run module
  *
  */
-import ray from './index.js';
-const response = await ray.spawn('ls');
-console.log(response);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_js_1 = __importDefault(require("./index.js"));
+async function main() {
+    const response = await index_js_1.default.spawn('ls');
+    console.log(response);
+}
+main();
 //# sourceMappingURL=run.js.map
